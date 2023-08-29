7d3bcee8c69ce1ad8744e7f7bb2b6c0821b3b438
cmesah <cmesahenao@gmail.com>
2023-08-29 16:15:18 -0500

    Se importa la clase Field para validar datos del model

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI,Body
 from fastapi.responses import HTMLResponse
-from pydantic import BaseModel
+from pydantic import BaseModel,Field
 from typing import Optional
 
 app = FastAPI()
@@ -9,9 +9,9 @@ app.version = "0.0.1"
 
 class Movie(BaseModel):
     id : Optional[int] = None
-    title : str
-    overview : str
-    year : str
+    title : str = Field(default= 'Mi pelicula',min_length= 5,max_length=15)
+    overview : str = Field(default= 'Descripción pelicula',min_length= 15,max_length=50)
+    year : int = Field(default= 2022,le= 2023)
     rating: float
     category:str 
     
